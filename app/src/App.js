563ceded5106aaf4124bf2696bb4b39388997aa7
liav2002 <liavmail00@gmail.com>
2024-06-24 22:20:54 +0300

    implement basic for all new components

--- app/src/App.js
+++ app/src/App.js
@@ -3,6 +3,10 @@ import './App.css';
 import Login from './components/login';
 import Register from './components/register';
 import Home from './components/home';
+import Albums from './components/albums';
+import Info from './components/info';
+import Posts from './components/posts';
+import Todos from './components/todos';
 import { Route, BrowserRouter as Router, Redirect, Switch } from 'react-router-dom'
 
 function App() {
@@ -18,6 +22,11 @@ function App() {
           <Route exact path="/login" component={Login} />
           <Route path="/register" component={Register} />
           <Route path="/home" component={Home} />
+          <Route path="/albums" component={Albums} />
+          <Route path="/info" component={Info} />
+          <Route path="/posts" component={Posts} />
+          <Route path="/todos" component={Todos} />
+
         </Switch>
       </Router>
     </div>
---
+++ app/src/components/albums.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function Albums() {
+  const user = JSON.parse(localStorage.getItem('user'));
+
+  return (
+    <>
+      <div className="container">
+        <div className="content">
+            <h1>Still In Develop</h1>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Albums;
